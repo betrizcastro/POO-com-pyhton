aecfda4f42729bd4796946e56a09dabbbf208d82
Beatriz Castro <mbdecastro2007@gmail.com>
2025-09-19 08:46:22 -0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from livro import Livro
 
 livro1 = Livro()
-livro1.definir_informacao("2020", "a vida invisível de addie larue")
+livro1.definir_informacao('A vida invisível de Addie Larue', "V.E. Schwab")
+
 livro1.exibir_informacao()
