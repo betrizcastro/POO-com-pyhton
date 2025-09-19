5c0c32640aa543646d570e16a20c75a90be80894
Beatriz Castro <mbdecastro2007@gmail.com>
2025-09-19 08:40:46 -0300

    Add files via upload

---
+++ livro.py
@@ -0,0 +1,10 @@
+class Livro:
+
+    def definir_informacao(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+
+    def exibir_informacao(self):
+        print(f'Título: {self.titulo}, Autor: {self.autor}')
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from livro import Livro
+
+livro1 = Livro()
+livro1.definir_informacao("2020", "a vida invisível de addie larue")
+livro1.exibir_informacao()
