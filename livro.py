d9ae7ddd1acadda03ba5219204ee56931338c684
Beatriz Castro <mbdecastro2007@gmail.com>
2025-09-19 08:53:50 -0300

    livro.py

--- livro.py
+++ livro.py
@@ -1,11 +1,14 @@
 class Livro:
-
+    titulo = ""
+    autor = ""
+    
     def definir_informacao(self, titulo, autor):
         self.titulo = titulo
         self.autor = autor
 
     def exibir_informacao(self):
         print(f'Título: {self.titulo}, Autor: {self.autor}')
+
 
 
 
